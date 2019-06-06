91cf403a5d61a9797c9e0003e70bdb24f6e19087
Perkins <Genevieve.Perkins@gov.bc.ca>
2019-06-06 09:20:00 -0700

    update gitignore

--- 01_load.R
+++ 01_load.R
@@ -1,12 +1,20 @@
 # Copyright 2019 Province of British Columbia
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 # http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
+## Load in libraries
+
+x <- c("dplyr","ggplot2","tidyr","stringr","here","reshape") #raster","sp","sf","rgdal","xlsx","rJava","tibble","mapview","gtools")
+lapply(x, library, character.only = TRUE) ; rm(x)  # load the required packages
+
+## Load  data files
+
+data.dir <- "C:/Temp/01.Indicators/01.EPR/"
