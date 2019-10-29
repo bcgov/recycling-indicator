f55abe47280a4f8e3d3e0118652fc084035e69a1
GPerkins <Genevieve.Perkins@gov.bc.ca>
2019-10-29 15:42:32 -0700

    tidy file

--- 01_load.R
+++ 01_load.R
@@ -11,7 +11,6 @@
 # See the License for the specific language governing permissions and limitations under the License.
 
 ## Load in libraries
-library(ggplot2)
 library(readr)
 library(dplyr)
 library(envreportutils)
@@ -75,4 +74,5 @@ reg_dist <- combine_nr_rd() %>%
   st_transform(4326) %>%
   group_by(ADMIN_AREA_NAME,ADMIN_AREA_ABBREVIATION) %>%
   summarize()
+
 
